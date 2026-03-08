dd12dd25901d9db3b19578600e7ff8f349b4b998
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/performances/page.tsx
+++ app/performances/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import PerformanceCard from '../components/PerformanceCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Performances | Rhythm & Flow Dance Studio',
